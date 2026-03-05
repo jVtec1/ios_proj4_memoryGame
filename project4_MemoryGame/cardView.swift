48c1685ba5b2c0603ad51ee259a93d64a2b7d112
Andy Espinoza <andyesp831@gmail.com>
2026-03-04 20:33:45 -0800

    Initi commit

---
+++ project4_MemoryGame/cardView.swift
@@ -0,0 +1,18 @@
+//
+//  cardView.swift
+//  project4_MemoryGame
+//
+//  Created by Andy Espinoza on 3/4/26.
+//
+
+import SwiftUI
+
+struct cardView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    cardView()
+}
